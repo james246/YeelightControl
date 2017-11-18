e86d6f5c0aa4db8f65c6591754e436c7c9cf6abc
James Paterson <paterson.js@gmail.com>
2017-11-18 19:54:32 +0000

    Initial commit

---
+++ YeelightControl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YeelightControl
+//
+//  Created by James on 18/11/2017.
+//  Copyright © 2017 James. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YeelightControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YeelightControl
+//
+//  Created by James on 18/11/2017.
+//  Copyright © 2017 James. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
